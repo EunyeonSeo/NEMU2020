907648d9e023ffe78a7a049c4dc82ee3910a466d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-06 08:39:16 +0000

    >  compile NEMU 3019244264 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:39:16 up  2:28,  1 user,  load average: 0.41, 0.14, 0.11 78c37a8a5e85515a684a1445b76c7daad7d7ed9a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,8 +6,15 @@
 #include <sys/types.h>
 #include <regex.h>
 
+#include <stdlib.h>
+#include <ctype.h>
+#include <setjmp.h>
+
+jmp_buf env_buf;
+
+enum { EPARE = 1, EREG, ESYN, ESYM };
 enum {
-	NOTYPE = 256, EQ
+	NOTYPE = 256, EQ, DEC, HEX, REG, NEQ, AND, OR, NOT, DEREF, VAR
 
 	/* TODO: Add more token types */
 
@@ -24,7 +31,23 @@ static struct rule {
 
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
-	{"==", EQ}						// equal
+	{"\\-", '-'},					// subtraction
+	{"\\*", '*'},					// multiplication
+	{"/", '/'},					// division
+	{"\\(", '('},					// left-parenthese
+	{"\\)", ')'},					// right-parenthese
+	{"0[xX][0-9a-fA-F]+", HEX},			// hex number
+	{"[0-9][0-9]*", DEC},				// decmical number
+	{"\\$[A-Za-z]+", REG},				// register
+	{"==", EQ},					// equal
+	{"!=", NEQ},					// not equal
+	{"&&", AND},					// logic and
+	{"\\|\\|", OR},					// logic or
+	{"!", NOT},					// logic not
+	{"[_a-zA-Z][_0-9a-zA-Z]*", VAR}			// variable
+	
+
+
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -77,11 +100,38 @@ static bool make_token(char *e) {
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
 				 */
+				
 
-				switch(rules[i].token_type) {
-					default: panic("please implement me");
+				if(nr_token == 32){
+					fprintf(stderr, "The expression is too long.");
+					return false;
 				}
 
+				if(substr_len > 31){
+					fprintf(stderr, "buffer overflow\n");
+					return false;
+				}
+
+				if(rules[i].token_type == NOTYPE) break;
+
+				tokens[nr_token].type = rules[i].token_type;
+
+				switch(rules[i].token_type) {
+					case DEC: case HEX: case VAR:
+					strncpy(tokens[nr_token].str, substr_start, substr_len);
+					tokens[nr_token].str[substr_len] = '\0';
+					break;
+
+					case REG:
+					//drop the leading '$'
+					strncpy(tokens[nr_token].str, substr_start + 1, substr_len - 1);
+					tokens[nr_token].str[substr_len-1] = '\0';
+					break;
+					default:// panic("please implement me"); 
+						; 
+				}
+
+				++nr_token;
 				break;
 			}
 		}
@@ -95,7 +145,231 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+
+//check whether the expr is surrounded by a matched pair of parentheses
+bool check_parentheses(int p, int q){
+	int count = 0;
+	bool empty = false;
+
+	int ix;
+	for(ix = p; ix <= q; ix++){
+		if (tokens[ix].type == '(') ++count;
+
+		else if (tokens[ix].type == ')') {
+			--count;
+			if (count < 0) longjmp (env_buf, EPARE);
+			else if (count == 0 && ix != q) empty = true; //expr is not surrounded by a pair of parentheses
+		}
+	}
+
+	if (count > 0)
+		longjmp(env_buf, EPARE);
+
+	if (tokens[p].type == '(' && tokens[q].type == ')' && !empty)
+		return true;
+
+	else return false;
+
+}
+
+bool is_operator (int type){
+	switch (type) {
+		case'+': case'-': case '*': case'/':
+		case EQ: case NEQ: case AND: case OR: case NOT: case DEREF:
+			return true;
+
+		default:
+			return false;
+	}
+}
+
+bool inside_pare (int ix, int begin, int end){
+	/*use stack to check whether the operator indexed by ix is inside a pair of pare*/
+
+	assert(begin <= ix && ix <= end);
+
+	int count = 0;
+	while (begin < ix) {
+		if (tokens[begin].type == '(') ++count;
+		else if (tokens[begin].type == ')') --count;
+		++begin;
+	}
+
+	if (count > 0) return true;
+	else return false;
+}
+
+//the begger the value, the lower the precedence
+int preced (int type) {
+	switch (type) {
+		case NOT: case DEREF:
+			return 1;
+		case '*': case '/':
+			return 2;
+		case '+': case '-':
+			return 3;
+		case EQ: case NEQ:
+			return 4;
+		case AND: return 5;
+		case OR: return 6;
+		default: assert(0);
+	}
+
+}
+
+int preced_comp (int type1, int type2){
+	
+	return preced(type1) - preced(type2);
+
+}
+
+enum {LEFT, RIGHT};
+
+//associativity of a operator
+int associate(int type){
+	switch (type) {
+		case '+': case'-': case'*': case'/':
+		case EQ: case NEQ: case AND: case OR:
+			return LEFT;
+		case NOT: case DEREF:
+			return RIGHT;
+		default: assert(0);
+	}
+
+}
+
+int dominant_operator(int p, int q){
+	panic("Waiting for implemention");
+
+	//find the first operator that is not inside parentheses
+	int ix = p;
+	while (ix <= q && (!is_operator(tokens[ix].type) || inside_pare(ix, p, q) ) )
+		++ix;
+
+	if (ix > q) longjmp (env_buf, ESYN);
+
+	int ix_domin_oper = ix;
+
+	for (++ix; ix <= q; ++ix) {
+		if (!is_operator(tokens[ix].type) || inside_pare(ix, p, q))
+			continue;
+
+		//current operator has lower or same precedence and left-associative
+		int cur_type = tokens[ix].type;
+		int domin_type = tokens[ix_domin_oper].type;
+		if (preced_comp(cur_type, domin_type) > 0 || (preced_comp(cur_type, domin_type) == 0 && associate(cur_type) == LEFT))
+			ix_domin_oper = ix;
+
+	}
+
+	return ix_domin_oper;
+
+}
+
+uint32_t reg_val (char *reg_name){
+	//convert to lowercase
+//	char *pstr;
+//	for (*pstr = reg_name; *pstr != '\0'; ++pstr)
+//		*pstr = tolower(*pstr);
+
+	int ix;
+	for (ix = R_EAX; ix <= R_EDI; ++ix){
+		if (!strcmp(reg_name, regsl[ix]))
+			return reg_l(ix);
+	}
+
+	for (ix = R_AX; ix <= R_DI; ++ix){
+		if (!strcmp(reg_name, regsw[ix]))
+			return reg_w(ix);
+	}
+
+	for (ix = R_AL; ix <= R_BH; ++ix){
+		if (!strcmp(reg_name, regsb[ix]))
+			return reg_b(ix);
+	}
+
+	if (!strcmp(reg_name, "eip"))
+		return cpu.eip;
+
+	else longjmp (env_buf, EREG);
+	
+
+} 
+
+
+uint32_t eval (int p, int q){
+	if(p > q) {
+		//bad expression
+		longjmp (env_buf, ESYN);
+	}
+
+	else if (p == q){
+		//single token
+		//a decimal num, hex num, or name of register
+		//return the value of it
+
+		switch (tokens[p].type) {
+			case DEC: return atoi(tokens[p].str);
+			case HEX: {
+				uint32_t tmp;
+				sscanf(tokens[p].str, "%x", &tmp);
+				return tmp;
+			}
+			case REG:
+				return reg_val(tokens[p].str);
+			default: longjmp (env_buf, ESYN);
+		}
+	}
+
+	else if (check_parentheses (p, q) == true){
+		//if surrounded by a matched pair of parentheses
+		//just throw away the parentheses
+		return eval(p+1, q-1);
+
+	}
+	else {
+		int op = dominant_operator(p,q);
+		//the position of dominant operator in the token expression
+		int op_type = tokens[op].type;
+
+		//unary operators
+		if (op_type == NOT || op_type == DEREF){
+			uint32_t val = eval(op+1, q);
+			switch (tokens[op].type) {
+				case NOT: return !val;
+				case DEREF:
+					return swaddr_read(val, 4);
+				default: assert(0);
+			}
+		}
+
+		//binary operators
+		else{
+			uint32_t val1 = eval(p, op-1);
+			uint32_t val2 = eval(op+1, q);
+
+			switch (tokens[op].type) {
+				case '+': return val1 + val2;
+				case '-': return val1 - val2;
+				case '*': return val1 * val2;
+				case '/': return val1 / val2;
+				case EQ: return val1 == val2;
+				case NEQ: return val1 != val2;
+				case AND: return val1 && val2;
+				case OR: return val1 || val2;
+
+				default: assert(0);
+			}
+		}
+
+	}
+}
 uint32_t expr(char *e, bool *success) {
+
+	assert(0);
+	
+	*success = true;	
+
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
@@ -103,6 +377,55 @@ uint32_t expr(char *e, bool *success) {
 
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
-	return 0;
+
+	//distinguish between DEREF and '*'
+	if (tokens[0].type == '*') {
+		tokens[0].type = DEREF;
+		//Log("tokens[%d].type = DEREF", 0);
+
+	}
+
+	int i; 
+	for (i = 1; i < nr_token; i++){
+ 	if (tokens[i].type == '*'){
+
+		int prev_t = tokens[i - 1].type;
+		int is_mul = prev_t == DEC || prev_t == HEX || prev_t == REG || prev_t == ')';
+		if (!is_mul) {
+
+			tokens[i].type = DEREF;
+			//Log("tokens[%d].type = DEREF", i);
+
+		}
+
+	}
+
 }
+
+	int ret = setjmp(env_buf);
+	if (ret == 0) return eval(0, nr_token-1);
+
+	else{
+	//an exception has happened
+	*success = false;
+	switch (ret){
+		case EPARE:
+			fprintf(stderr, "Parentheses do not match\n");
+			break;
+		case EREG:
+			fprintf(stderr, "no such register\n");
+			break;
+		case ESYN:
+			fprintf(stderr, "synatax error\n");
+			break;
+		case ESYM:
+			break;
+		default:
+			assert(0);
+	}
+		return 0;  
+	}
+	
+}
+
 
