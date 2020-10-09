c30473baf47762ce4f656397c27b3f43d8f4c8f8
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 06:37:36 +0000

    >  compile NEMU 3019244264 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:37:36 up 18:21,  1 user,  load average: 0.69, 0.32, 0.16 e77646689a69a3666e7ed6af8353490326b2d131

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -2,7 +2,7 @@
 #include "cpu/helper.h"
 #include <setjmp.h>
 #include "monitor/watchpoint.h"
-
+#include "cpu/reg.h"
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
